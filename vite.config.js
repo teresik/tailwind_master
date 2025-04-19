3746adc287c9c0bdd3bf222e82cabf0911ad9c64
Teresik <sheff20.152@gmail.com>
2025-04-19 09:39:34 +0200

    Set base URL in Vite configuration
    Added the `base` option to Vite's config, pointing to `/my-tailwind-project/`. This ensures proper asset resolution when deploying the project to a subdirectory.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+	base: '/my-tailwind-project/',
+
 	plugins: [
 		tailwindcss({
 			config: './src/css/tailwind.config.css'
